776aa2456ed0943486c7674eb261595d666584bd
unknown <matiasdiazarriaza@gmail.com>
2021-01-20 11:57:49 -0300

    Pagina Alerta terminada

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,50 @@ class AlertPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Alertas")),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlert(context),
+        ),
+      ),
     );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text("Alerta"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                    "Esto es un texto, que puede ser cualquier cosa como un texto"),
+                FlutterLogo(
+                  size: 100.0,
+                ),
+              ],
+            ),
+            actions: [
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 }
