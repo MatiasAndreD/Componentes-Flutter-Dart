f6660d52a9690470b3fe8041452e9bb8a37be268
unknown <matiasdiazarriaza@gmail.com>
2021-01-21 22:01:22 -0300

    Inputs terminados

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -6,7 +6,16 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _nombre;
+  String _nombre = "";
+  String _email = "";
+  String _pass = "";
+  String _date = "";
+  String _opcionSeleccionada = 'Volar';
+
+  List<String> _poderes = ['Volar', 'Fuerza', 'Rayos X', 'Ninguno'];
+
+  TextEditingController _inputFieldDateController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +26,15 @@ class _InputPageState extends State<InputPage> {
           padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
           children: [
             _crearInputs(),
+            Divider(),
+            _crearEmail(),
+            Divider(),
+            _inputPassword(),
+            Divider(),
+            _inputDate(context),
+            Divider(),
+            _crearDropdown(),
+            Divider(),
             _crearNombres(),
           ],
         ));
@@ -42,9 +60,122 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          counter: Text('Letra ' + _email.length.toString()),
+          hintText: 'Email',
+          labelText: 'Email',
+          helperText: 'Pon tu correo electronico',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email)),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+
+  Widget _inputPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Password',
+          labelText: 'Password',
+          helperText: 'Pon una clave para tu cuenta',
+          suffixIcon: Icon(Icons.vpn_key),
+          icon: Icon(Icons.lock)),
+      onChanged: (valor) {
+        setState(() {
+          _pass = valor;
+        });
+      },
+    );
+  }
+
+  Widget _inputDate(BuildContext context) {
+    return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputFieldDateController,
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
+      decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Fecha de nacimiento',
+          labelText: 'Fecha de nacimiento',
+          helperText: 'Fecha de nacimiento',
+          suffixIcon: Icon(Icons.calendar_today),
+          icon: Icon(Icons.calendar_view_day)),
+      onChanged: (valor) {
+        setState(
+          () {
+            _date = valor;
+            _inputFieldDateController.text = _date;
+          },
+        );
+      },
+    );
+  }
+
+  _selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(1930),
+        lastDate: new DateTime(2023),
+        locale: Locale('es', 'ES'));
+
+    if (picked != null) {
+      setState(() {
+        _date = picked.toString();
+        _inputFieldDateController.text = _date;
+      });
+    }
+  }
+
   Widget _crearNombres() {
     return ListTile(
       title: Text("El nombre es: $_nombre"),
+      subtitle: Text("Email:$_email y la fecha: " + _date.toString()),
+      trailing: Text(_opcionSeleccionada),
+    );
+  }
+
+  List<DropdownMenuItem<String>> getOpciones() {
+    List<DropdownMenuItem<String>> lista = new List();
+    _poderes.forEach((poder) {
+      lista.add(DropdownMenuItem(
+        child: Text(poder),
+        value: poder,
+      ));
+    });
+
+    return lista;
+  }
+
+  Widget _crearDropdown() {
+    return Row(
+      children: [
+        Icon(Icons.power),
+        SizedBox(
+          width: 30.0,
+        ),
+        DropdownButton(
+          value: _opcionSeleccionada,
+          items: getOpciones(),
+          onChanged: (opt) {
+            setState(() {
+              _opcionSeleccionada = opt;
+            });
+          },
+        )
+      ],
     );
   }
 }
